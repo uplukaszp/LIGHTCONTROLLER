8d6d4786c3b6c0aebb9dda8a14d3b98842e4a035
Lukasz <uplukaszp1@gmail.com>
2017-11-29 19:09:17 +0100

    Added colors

--- LIGHTCONTROLLER.cpp
+++ LIGHTCONTROLLER.cpp
@@ -11,123 +11,159 @@ decode_results results;
 Leds_Operator op;
 codes lastCode;
 void setup() {
-	lastCode=0;
+	lastCode = 0;
 	op.update();
 	Serial.begin(9600);
 	irrecv.enableIRIn();
 }
 void loop() {
-	 if(irrecv.decode(&results)) {
+	if (irrecv.decode(&results)) {
 		codes c;
-		if(results.value==REPEAT)
-		{
-			c=lastCode;
-			if(c==onOff||c==Fade3)c=0xFFFFF;
-			delay(500);
+		if (results.value == REPEAT) {
+			c = lastCode;
+			if (c == onOff || c == Fade3)
+				c = 0xFFFFF;
+			delay(200);
+		} else {
+			c = results.value;
 		}
-		else
-		{
-			c=results.value;
-		}
-		lastCode=c;
-		Serial.println(results.value,HEX);
-		Serial.println(c,HEX);
-		switch(c)
-		{
-		case 	brUp:
+		lastCode = c;
+		Serial.println(results.value, HEX);
+		Serial.println(c, HEX);
+		switch (c) {
+		case brUp:
 			op.incrBrightness();
 			break;
-		case 	brDn:
+		case brDn:
 			op.decrBrightness();
 			break;
-		case	onOff:
+		case onOff:
 			op.switchOnOff();
 			break;
-		case	next:
+		case next:
 			op.load();
 			break;
-		case	R:
+		case R:
 			op.setColor(CRGB::Red);
 			break;
-		case	R1:
-		case	R2:
-		case	R3:
-		case	R4:
-		case	RUp:
+		case R1:
+			op.setColor(CRGB::IndianRed);
+			break;
+		case R2:
+			op.setColor(CRGB::Crimson);
+			break;
+		case R3:
+			op.setColor(CRGB::Orange);
+			break;
+		case R4:
+			op.setColor(CRGB::Yellow);
+			break;
+		case RUp:
 			op.incrColor(CRGB::Red);
 			break;
-		case	RDn:
+		case RDn:
 			op.decrColor(CRGB::Red);
 			break;
-		case	G:
+		case G:
 			op.setColor(CRGB::Green);
 			break;
-		case	G1:
-		case	G2:
-		case	G3:
-		case	G4:
-		case	GUp:
+		case G1:
+			op.setColor(CRGB::LawnGreen);
+			break;
+		case G2:
+			op.setColor(CRGB::DarkCyan);
+			break;
+		case G3:
+			op.setColor(CRGB::Olive);
+			break;
+		case G4:
+			op.setColor(CRGB::SeaGreen);
+			break;
+		case GUp:
 			op.incrColor(CRGB::Green);
 			break;
-		case	GDn:
+		case GDn:
 			op.decrColor(CRGB::Green);
 			break;
-		case	B:
+		case B:
 			op.setColor(CRGB::Blue);
 			break;
-		case	Blue1:
-		case	B2:
-		case	B3:
-		case	B4:
-		case	BUp:
+		case Blue1:
+			op.setColor(CRGB::Aqua);
+			break;
+		case B2:
+			op.setColor(CRGB::MidnightBlue);
+			break;
+		case B3:
+			op.setColor(CRGB::DarkViolet);
+			break;
+		case B4:
+			op.setColor(CRGB::Fuchsia);
+			break;
+		case BUp:
 			op.incrColor(CRGB::Blue);
 			break;
-		case	BDn:
+		case BDn:
 			op.decrColor(CRGB::Blue);
 			break;
-		case	W:
+		case W:
 			op.setColor(CRGB::White);
 			break;
-		case	W1:
-		case	W2:
-		case	W3:
-		case	W4:
-		case	Quick:
-		case	Slow:
-		case	Diy1:
+		case W1:
+			op.setColor(CRGB::Bisque);
+			break;
+		case W2:
+			op.setColor(CRGB::Khaki);
+			break;
+		case W3:
+			op.setColor(CRGB::LightBlue);
+			break;
+		case W4:
+			op.setColor(CRGB::LightSlateGray);
+			break;
+		case Quick:
+			break;
+		case Slow:
+			break;
+		case Diy1:
 			op.HSV();
 			break;
-		case	Diy2:
-		case	Diy3:
-		case	Diy4:
+		case Diy2:
+			break;
+		case Diy3:
+			break;
+		case Diy4:
 			op.HSV(true);
 			break;
-		case	Diy5:
-		case	Diy6:
-		case	Auto:
-		case	Flash:
-		case	Jump3:
+		case Diy5:
+			break;
+		case Diy6:
+			break;
+		case Auto:
+			break;
+		case Flash:
+			break;
+		case Jump3:
 			op.setAnimation(1);
 			break;
-		case	Jump7:
+		case Jump7:
 			op.setAnimation(2);
 			break;
-		case	Fade3:
+		case Fade3:
 			op.nextPalette();
 			break;
-		case	Fade7:
+		case Fade7:
 			op.save();
 			break;
 		default:
 			break;
 		}
 		irrecv.resume();
-	 }
-	 if(irrecv.isIdle())
-	 {
-		 op.nextFrame();
-		 op.update();
-	 }
+	}
+	if (irrecv.isIdle()) {
+		op.nextFrame();
+		op.update();
+	}
 
 }
 
