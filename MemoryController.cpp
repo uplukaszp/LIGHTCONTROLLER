754787a6f9b884da626e1fae92387e293d00cc03
Lukasz <uplukaszp1@gmail.com>
2017-12-08 14:18:59 +0100

    Fixed some bugs

--- MemoryController.cpp
+++ MemoryController.cpp
@@ -7,29 +7,49 @@
 
 #include "MemoryController.h"
 Memory_Controller::Memory_Controller() {
+	EEPROM.setMaxAllowedWrites(1000);
 }
 
 void Memory_Controller::save(CRGB* leds) {
 
-	int _address=100;
+	int _address=1;
 	Serial.println("save: ");
 	for(int i=0;i<NUM_LEDS;i++)
 	{
+		Serial.print("\ntoSaveR:");
+		Serial.print(leds[i].r);
+		Serial.print("\t Saved: ");
+
 		EEPROM.update(_address++,leds[i].r);
+		Serial.print(EEPROM.readByte(_address-1));
+		Serial.print("\ntoSaveG:");
+		Serial.print(leds[i].g);
+		Serial.print("\t Saved: ");
 		EEPROM.update(_address++,leds[i].g);
+		Serial.print(EEPROM.readByte(_address-1));
+		Serial.print("\ntoSaveB:");
+		Serial.print(leds[i].b);
+		Serial.print("\t Saved: ");
 		EEPROM.update(_address++,leds[i].b);
+		Serial.print(EEPROM.readByte(_address-1));
 	}
 }
 
 CRGB* Memory_Controller::load() {
 	CRGB *leds=new CRGB[NUM_LEDS];
-	int _address=100;
+	int _address=1;
 	Serial.println("load");
 	for(int i=0;i<NUM_LEDS;i++)
 		{
 			leds[i].r=EEPROM.readByte(_address++);
+			Serial.print("\nReadedR:");
+			Serial.print(leds[i].r);
 			leds[i].g=EEPROM.readByte(_address++);
+			Serial.print("\tReadedG:");
+			Serial.print(leds[i].g);
 			leds[i].b=EEPROM.readByte(_address++);
+			Serial.print("\tReadedB:");
+			Serial.print(leds[i].b);
 		}
 
 	return leds;
